4700c5e88e3e9bb9739b47fb86fbeecf12f76e8d
ArealtemDeveloper <seriousguytuwan@gmail.com>
2022-10-01 19:43:25 +0500

    Initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,64 @@
+import webpack from 'webpack'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BuildOptions } from './types/config'
+
+export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const fileLoader =
+      {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+              {
+                  loader: 'file-loader'
+              }
+          ]
+      }
+
+    const babelLoader = {
+        test: /\.(js|ts|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    }
+
+    const svgLoader = {
+        test: /\.svg$/i,
+        use: ['@svgr/webpack']
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.scss')),
+                        localIdentName: isDev
+                            ? '[path][name]___[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    }
+                }
+            },
+            'sass-loader'
+        ]
+    }
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+    }
+
+    return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
+        typescriptLoader,
+        cssLoader
+    ]
+}
